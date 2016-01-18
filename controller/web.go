4b0552639cd74909a01ba28f7859943559e77c7a
novalagung <caknopal@gmail.com>
2016-01-18 13:58:08 +0700

    no message

--- controller/web.go
+++ controller/web.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/eaciit/knot/knot.v1"
 )
 
@@ -21,7 +20,5 @@ func (w *WebController) Index(r *knot.WebContext) interface{} {
 	r.Config.IncludeFiles = IncludeFiles
 	r.Config.ViewName = "views/index.html"
 
-	fmt.Println(LayoutFile, IncludeFiles)
-
-	return "Asdfsadfs"
+	return true
 }
