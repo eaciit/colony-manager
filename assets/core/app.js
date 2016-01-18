0b6e5e38b5f97cbd6a036d2433cf3b1584ccc027
novalagung <caknopal@gmail.com>
2016-01-18 19:16:46 +0700

    no message

--- assets/core/app.js
+++ assets/core/app.js
@@ -1,1 +1,20 @@
-var viewModel = {};
+viewModel.app = {}; var app = viewModel.app;
+
+app.applyNavigationActive = function () {
+	var currentURL = document.URL.split("/").slice(3).join("/");
+	var $a = $("a[href='/" + currentURL + "']");
+
+	$a.closest(".navbar-nav").children().removeClass("active");
+	if ($a.closest("li").parent().hasClass("navbar-nav")) {
+		$a.closest("li").addClass("active");
+	} else {
+		$a.closest("li").closest("li").addClass("active");
+	}
+};
+app.prepare = function () {
+	app.applyNavigationActive();
+};
+
+$(function () {
+	app.prepare();
+});
