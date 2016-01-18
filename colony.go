d86d880fa9bf445c01129be620af45a7d704338c
novalagung <caknopal@gmail.com>
2016-01-18 14:48:06 +0700

    no message

--- colony.go
+++ colony.go
@@ -3,58 +3,22 @@ package main
 import (
 	"github.com/eaciit/colony-manager/controller"
 	"github.com/eaciit/knot/knot.v1"
-	"os"
-	"os/exec"
-	"time"
+	"net/http"
+	"path"
 )
 
-type (
-	ColonyManager struct {
-		appViewsPath string
-		layoutFile   string
-		includeFiles []string
-		Server       *knot.Server
-
-		templateController *controller.TemplateController
-		pageController     *controller.PageController
-	}
+var (
+	server *knot.Server
 )
-
-func InitColonyManager() *ColonyManager {
-	yo := new(ColonyManager)
-
-	// prepare view path
-	v, _ := os.Getwd()
-	yo.appViewsPath = v + "/"
-	yo.layoutFile = "views/layout.html"
-	yo.includeFiles = []string{"views/_head.html"}
-	yo.Server = new(knot.Server)
-
-	knot.DefaultOutputType = knot.OutputTemplate
-
-	// initiate controller
-	yo.templateController = &controller.TemplateController{yo.appViewsPath, yo.Server, yo.layoutFile, yo.includeFiles}
-	yo.pageController = &controller.PageController{yo.appViewsPath}
-
-	// initiate server
-	yo.Server.Address = "localhost:3000"
-	yo.Server.RouteStatic("static", yo.appViewsPath)
-	yo.templateController.RegisterRoutes()
-	yo.Server.Register(yo.templateController, "")
-	yo.Server.Register(yo.pageController, "")
-
-	return yo
-}
-
-func (t *ColonyManager) Open() {
-	time.AfterFunc(time.Second, func() {
-		exec.Command("open", "http://"+t.Server.Address).Run()
-	})
-}
 
 func main() {
-	yo := InitColonyManager()
-	yo.Server.Address = "localhost:8787"
-	// yo.Open()
-	yo.Server.Listen()
+	server = new(knot.Server)
+	server.Address = "localhost:3000"
+	server.RouteStatic("res", path.Join(controller.AppViewPath, "assets"))
+	server.Register(controller.CreateWebController(server), "")
+	server.Route("/", func(r *knot.WebContext) interface{} {
+		http.Redirect(r.Writer, r.Request, "/web/index", 301)
+		return true
+	})
+	server.Listen()
 }
