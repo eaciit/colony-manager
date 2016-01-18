e539fd619b0f3fa4f12fa0615d89c0c1e636c6a8
novalagung <caknopal@gmail.com>
2016-01-18 16:57:34 +0700

    no message

--- controller/web.go
+++ controller/web.go
@@ -18,7 +18,16 @@ func (w *WebController) Index(r *knot.WebContext) interface{} {
 	r.Config.OutputType = knot.OutputTemplate
 	r.Config.LayoutTemplate = LayoutFile
 	r.Config.IncludeFiles = IncludeFiles
-	r.Config.ViewName = "views/index.html"
+	r.Config.ViewName = "views/page-index.html"
+
+	return true
+}
+
+func (w *WebController) DataSource(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputTemplate
+	r.Config.LayoutTemplate = LayoutFile
+	r.Config.IncludeFiles = IncludeFiles
+	r.Config.ViewName = "views/page-datasource.html"
 
 	return true
 }
