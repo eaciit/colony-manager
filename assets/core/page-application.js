7d4f33924456132a6b25f2cbac3f5a3e4bd3695e
ranggaeaciit <rangga.hendradita@eaciit.com>
2016-02-04 14:44:42 +0700

    update
    added getapps controller
    added selectapps controller

--- assets/core/page-application.js
+++ assets/core/page-application.js
@@ -1,3 +1,46 @@
 app.section('scrapper');
 
 viewModel.application = {}; var apl = viewModel.application;
+
+apl.scrapperMode = ko.observable('');
+apl.scrapperData = ko.observableArray([]);
+apl.scrapperColumns = ko.observableArray([
+	{ field: "_id", title: "ID", width: 80 },
+	{ field: "AppsName", title: "Application Name", width: 130},
+	{ title: "", width: 80, attributes: { style: "text-align: center;" }, template: function (d) {
+		return [
+			"<button class='btn btn-sm btn-default btn-text-success btn-start tooltipster' title='Start Transformation Service' onclick='apl.runTransformation(\"" + d._id + "\")()'><span class='glyphicon glyphicon-play'></span></button>",
+			"<button class='btn btn-sm btn-default btn-text-primary tooltipster' title='Edit Application' onclick='apl.editScrapper(\"" + d._id + "\")'><span class='fa fa-pencil'></span></button>",
+			"<button class='btn btn-sm btn-default btn-text-danger tooltipster' title='Delete Application' onclick='apl.removeScrapper(\"" + d._id + "\")'><span class='glyphicon glyphicon-remove'></span></button>"
+		].join(" ");
+	} },
+	{ title: "Status", width: 80, attributes: { class:'scrapper-status' }, template: "<span></span>", headerTemplate: "<center>Status</center>" }
+	
+]);
+
+apl.getApplications = function() {
+	apl.scrapperData([]);
+	app.ajaxPost("/application/getapps", {}, function (res) {
+		if (!app.isFine(res)) {
+			return;
+		}
+
+		apl.scrapperData(res.data);
+	});
+};
+
+apl.editScrapper = function(_id) {
+	app.ajaxPost("/application/selectapps", { _id: _id }, function(res) {
+		if (!app.isFine(res)) {
+			return;
+		}
+
+		app.mode('editor');
+		apl.scrapperMode('edit');
+		console.log(res);
+	});
+};
+
+$(function () {
+	apl.getApplications();
+});
--- controller/application.go
+++ controller/application.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"github.com/eaciit/colony-core/v0"
+	"github.com/eaciit/colony-manager/helper"
 	"github.com/eaciit/knot/knot.v1"
+	// "github.com/eaciit/toolkit"
 )
 
 type ApplicationController struct {
@@ -12,4 +15,39 @@ func CreateApplicationController(s *knot.Server) *ApplicationController {
 	var controller = new(ApplicationController)
 	controller.Server = s
 	return controller
+}
+
+func (a *ApplicationController) GetApps(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputJson
+
+	cursor, err := colonycore.Find(new(colonycore.Application), nil)
+	if err != nil {
+		return helper.CreateResult(false, nil, err.Error())
+	}
+
+	data := []colonycore.Application{}
+	err = cursor.Fetch(&data, 0, false)
+	if err != nil {
+		return helper.CreateResult(false, nil, err.Error())
+	}
+	defer cursor.Close()
+
+	return helper.CreateResult(true, data, "")
+}
+
+func (a *ApplicationController) SelectApps(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputJson
+
+	payload := new(colonycore.Application)
+	err := r.GetPayload(payload)
+	if err != nil {
+		return helper.CreateResult(false, nil, err.Error())
+	}
+
+	err = colonycore.Get(payload, payload.ID)
+	if err != nil {
+		return helper.CreateResult(false, nil, err.Error())
+	}
+
+	return helper.CreateResult(true, payload, "")
 }
