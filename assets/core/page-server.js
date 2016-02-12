f9a0f05a4043c099daa1b52aae5aae6675aa7668
Ädinda Martha <ndte913@gmail.com>
2016-02-12 16:39:37 +0700

    no message

--- assets/core/page-server.js
+++ assets/core/page-server.js
@@ -18,17 +18,25 @@ srv.templateConfigServer = {
 	cmdcopy: "",
 	cmddir: ""
 };
+srv.templatetype = ko.observableArray([
+	{ value: "Local", text: "Local" },
+	{ value: "Remote", text: "Remote" }
+]);
+srv.templatetypeSSH = ko.observableArray([
+	{ value: "Credentials", text: "Credentials" },
+	{ value: "File", text: "File" }
+]);
 srv.filterValue = ko.observable('');
 srv.configServer = ko.mapping.fromJS(srv.templateConfigServer);
 srv.showServer = ko.observable(true);
 srv.ServerMode = ko.observable('');
 srv.ServerData = ko.observableArray([]);
 srv.ServerColumns = ko.observableArray([
-	{title: "<center><input type='checkbox' id='selectall' databind='click:srv.checkall'/></center>", width: 20, attributes: { style: "text-align: center;" }, template: function (d) {
+	{ headerTemplate: "<input type='checkbox' class='servercheckall' onclick=\"srv.checkDeleteServer(this, 'serverall', 'all')\"/>", width:25, template: function (d) {
 		return [
-			"<input type='checkbox' id='select' name='select[]' value= " + d._id + ">"
+			"<input type='checkbox' class='servercheck' idcheck='"+d._id+"' onclick=\"srv.checkDeleteServer(this, 'server')\" />"
 		].join(" ");
-	}},
+	} },
 	// { field: "_id", title: "ID", width: 80, template:function (d) { return ["<a onclick='srv.editServer(\"" + d._id + "\")'>" + d._id + "</a>"]} },
 	{ field: "_id", title: "ID", width: 80 },
 	{ field: "type", title: "Type", width: 80},
@@ -105,6 +113,30 @@ srv.editServer = function (_id) {
 		srv.ServerMode('edit');
 		ko.mapping.fromJS(res.data, srv.configServer);
 	});
+}
+
+srv.checkDeleteServer = function(elem, e){
+	if (e === 'serverall'){
+		if ($(elem).prop('checked') === true){
+			$('.servercheck').each(function(index) {
+				$(this).prop("checked", true);
+				srv.tempCheckIdServer.push($(this).attr('idcheck'));
+			});
+		} else {
+			var idtemp = '';
+			$('.servercheck').each(function(index) {
+				$(this).prop("checked", false);
+				idtemp = $(this).attr('idcheck');
+				srv.tempCheckIdServer.remove( function (item) { return item === idtemp; } );
+			});
+		}
+	}else {
+		if ($(elem).prop('checked') === true){
+			srv.tempCheckIdServer.push($(elem).attr('idcheck'));
+		} else {
+			srv.tempCheckIdServer.remove( function (item) { return item === $(elem).attr('idcheck'); } );
+		}
+	}
 }
 
 var vals = [];
