b6c0e6259f3368d87fa2b3fe65f86f60a4b94160
yanda15 <yanda151092@gmail.com>
2016-03-01 18:51:40 +0700

    file browser changes

--- assets/core/page-filebrowser.js
+++ assets/core/page-filebrowser.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
 
 $("#FileBrowser").ecFileBrowser({
         dataSource:{
-            url: 'https://gist.githubusercontent.com/yanda15/bf83bc831ca7363e5ec2/raw/ee63e18b7617dab8542ab8dbbabe946b44808329/Sample%2520data%2520File%2520Browser',
+            url: '//demos.telerik.com/kendo-ui/service/Employees',
             call: 'GET',
-            callData: 'search'
+            callData: 'search',
+            pathField: "EmployeeId",
+            hasChildrenField:"HasEmployees",
+            nameField:"FullName"
         }, 
-
         serverSource:{
              url: 'https://gist.githubusercontent.com/yanda15/cfcc16748f09bc6518fd/raw/c16bc1e411c9005d86d988cfce079fb018224036/Sample%2520Server%2520Data',
             call: 'GET',
@@ -17,23 +19,17 @@ $("#FileBrowser").ecFileBrowser({
 
 $("#FileBrowser2").ecFileBrowser({
         dataSource:{
-            data: [
-                        { text: "ABC", items: [
-                            { text: "Tables & Chairs.pdf" },
-                            { text: "Sofas.png" },
-                            { text: "Occasional Furniture.skype" }
-                        ] },
-                        { text: "DEF", items: [
-                            { text: "Bed Linen" },
-                            { text: "Curtains & Blinds" },
-                            { text: "Carpets" }
-                        ] }
-                    ]
+            url: '//demos.telerik.com/kendo-ui/service/Employees',
+            call: 'GET',
+            callData: 'search',
+            pathField: "EmployeeId",
+            hasChildrenField:"HasEmployees",
+            nameField:"FullName"
         }, 
         serverSource:{
              data:[
-                { text: "Server c"},
-                { text: "Server d"}
+                { "text": "Server c"},
+                { "text": "Server d"}
             ]
         }
     });
