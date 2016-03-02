43660e1107d885ee8e09d8fe4b47d79f1604cc76
yanda15 <yanda151092@gmail.com>
2016-03-02 11:34:45 +0700

    file browser changes

--- assets/core/page-filebrowser.js
+++ assets/core/page-filebrowser.js
@@ -5,15 +5,15 @@ $("#FileBrowser").ecFileBrowser({
         dataSource:{
             url: '//demos.telerik.com/kendo-ui/service/Employees',
             call: 'GET',
-            callData: 'search',
             pathField: "EmployeeId",
             hasChildrenField:"HasEmployees",
             nameField:"FullName"
         }, 
         serverSource:{
-             url: 'https://gist.githubusercontent.com/yanda15/cfcc16748f09bc6518fd/raw/c16bc1e411c9005d86d988cfce079fb018224036/Sample%2520Server%2520Data',
-            call: 'GET',
-            callData: 'search'
+             url: 'https://gist.githubusercontent.com/yanda15/cfcc16748f09bc6518fd/raw/2dab77f5b2b3ad3ec7e4edee31d04a11da17abdc/Sample%2520Server%2520Data',
+             call: 'GET',
+             dataTextField:"text",
+             dataValueField:"text"
         }
     });
 
