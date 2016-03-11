faf42c2dae189c33efd7a948126c8d79b458137e
nadnawidy <nanda.widy.h@gmail.com>
2016-03-11 11:03:42 +0700

    deploy windows
    deploy windows

---
+++ controller/group.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	// "archive/zip"
+	// "encoding/json"
+	// "fmt"
+	// "github.com/eaciit/colony-core/v0"
+	// "github.com/eaciit/colony-manager/helper"
+	// "github.com/eaciit/dbox"
+	// _ "github.com/eaciit/dbox/dbc/jsons"
+	"github.com/eaciit/knot/knot.v1"
+	// "github.com/eaciit/toolkit"
+	// "io"
+	// "io/ioutil"
+	// "os"
+	// "path/filepath"
+	// "strings"
+	// "time"
+)
+
+
+type AdministrationController struct {
+	App
+}
+
+func CreateAdminisrationController(s *knot.Server) *AdministrationController {
+	var controller = new(AdministrationController)
+	controller.Server = s
+	return controller
+}
