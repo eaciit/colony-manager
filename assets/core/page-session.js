c97f1fd234b279d3a1786b092b263560a6bae6cc
alipsidikp <alip.sidik.p@gmail.com>
2016-03-21 09:48:11 +0700

    Change in session

--- assets/core/page-session.js
+++ assets/core/page-session.js
@@ -3,17 +3,22 @@ app.section('session');
 viewModel.session = {}; var ses = viewModel.session;
   
 ses.SessionColumns = ko.observableArray([
-	{ template: "<input type='checkbox' name='checkboxsession' class='ckcGrid' value='#: _id #' />", width: 50  },
-	{ field: "_id", title: "ID" },
-	{ field: "userid", title: "Userid" },
-	{ field: "created", title: "Created" },
-	{ field: "expired", title: "Expired" },
-	{ title: "", width: 80, attributes: { class: "align-center" }, template: function (d) {
-		return [
-			"<button class='btn btn-sm btn-default btn-text-success tooltipster' onclick='ses.selectGridSession(\"" + d._id + "\")' title='destroy session'><span class='fa fa fa-times'></span></button>"
-		].join(" ");
-	} }
+	{ field: "status", title: "" },
+	{ field: "username", title: "Username" },
+	{ field: "created", title: "Created", template:'# if (created == "0001-01-01T00:00:00Z") {#-#} else {# #:moment(created).utc().format("DD-MMM-YYYY HH:mm:ss")# #}#' },
+	{ field: "expired", title: "Expired", template:'# if (expired == "0001-01-01T00:00:00Z") {#-#} else {# #:moment(expired).utc().format("DD-MMM-YYYY HH:mm:ss")# #}#' },
+	{ field: "duration", title: "Active In", template:'#= kendo.toString(duration, "n2")# H'},
+	{ title: "", width: 80, attributes: { class: "align-center" }, template:"#if(status=='EXPIRED'){# <button data-value='#:_id #' onclick='ses.setexpired(\"#: _id #\", \"#: username #\")' name='expired' type='button' class='btn btn-sm btn-default btn-text-danger btn-stop tooltipster' title='Set Expired'><span class='fa fa-stop'></span></button> #}else{# #}#" }
+	// { title: "", width: 80, attributes: { class: "align-center" }, template: function (d) {
+	// 	if (status == "ACTIVE") {
+	// 		return [
+	// 			"<button class='btn btn-sm btn-default btn-text-success tooltipster' onclick='ses.selectGridSession(\"" + d._id + "\")' title='Set Expired'><span class='fa fa fa-times'></span></button>"
+	// 		].join(" ");
+	// 	}
+	// 	return ""
+	// } }
 ]); 
+
 ses.SessionData=ko.observableArray([]);
 ses.selectGridSession = function (e) {
 	// adm.isNew(false);
@@ -47,7 +52,19 @@ ses.getSession = function(c) {
 	});
 };
  
- 
+ses.setexpired = function (_id,username) {
+	var param ={ _id: _id };
+	app.ajaxPost("/session/setexpired", param, function (res) {
+		if (!app.isFine(res)) {
+			return;
+		}
+
+		ses.getSession()
+		// wg.selectedID(_id);
+		// app.mode('history');
+		// wg.historyData(res.data);
+	});
+} 
  
 // ses.editGroup = function(c) {
 // 	var payload = ko.mapping.toJS(ses.filter._id(c));
--- controller/session.go
+++ controller/session.go
@@ -11,6 +11,7 @@ import (
 	// _ "github.com/eaciit/dbox/dbc/jsons"
 	"github.com/eaciit/knot/knot.v1"
 	"github.com/eaciit/toolkit"
+	"time"
 	// "io"
 	// "io/ioutil"
 	// "os"
@@ -37,8 +38,26 @@ func (a *SessionController) GetSession(r *knot.WebContext) interface{} {
 
 	arrm := make([]toolkit.M, 0, 0)
 	c, err := acl.Find(tSession, nil, toolkit.M{}.Set("take", 0))
-	if err == nil {
-		err = c.Fetch(&arrm, 0, false)
+	if err != nil {
+		return helper.CreateResult(true, nil, err.Error())
+	}
+
+	err = c.Fetch(&arrm, 0, false)
+	for i, val := range arrm {
+		tUser := new(acl.User)
+		e := acl.FindByID(tUser, toolkit.ToString(val.Get("userid", "")))
+		if e != nil {
+			continue
+		}
+		arrm[i].Set("username", tUser.LoginID)
+		arrm[i].Set("duration", time.Since(val["created"].(time.Time)).Hours())
+		arrm[i].Set("status", "ACTIVE")
+		if val["expired"].(time.Time).Before(time.Now().UTC()) {
+			arrm[i].Set("duration", val["expired"].(time.Time).Sub(val["created"].(time.Time)).Hours())
+			arrm[i].Set("status", "EXPIRED")
+		}
+		arrm[i].Set("username", tUser.LoginID)
+		toolkit.Printf("Debug date : %v : %v\n", toolkit.TypeName(val["created"]), val["created"].(time.Time))
 	}
 
 	if err != nil {
@@ -48,39 +67,40 @@ func (a *SessionController) GetSession(r *knot.WebContext) interface{} {
 	}
 
 }
-func (a *SessionController) FindSession(r *knot.WebContext) interface{} {
-	r.Config.OutputType = knot.OutputJson
-	a.InitialSetDatabase()
-	payload := map[string]interface{}{}
-	err := r.GetPayload(&payload)
-	tSession := new(acl.Session)
-	err = acl.FindByID(tSession, payload["_id"].(string))
-	if err != nil {
-		return helper.CreateResult(true, nil, err.Error())
-	} else {
-		return helper.CreateResult(true, tSession, "")
-	}
 
-}
-func (a *SessionController) DeleteSession(r *knot.WebContext) interface{} {
-	r.Config.OutputType = knot.OutputJson
-	a.InitialSetDatabase()
-	tSession := new(acl.Session)
-	payload := map[string]interface{}{}
-	err := r.GetPayload(&payload)
-	err = acl.FindByID(tSession, payload["_id"].(string))
+// func (a *SessionController) FindSession(r *knot.WebContext) interface{} {
+// 	r.Config.OutputType = knot.OutputJson
+// 	a.InitialSetDatabase()
+// 	payload := map[string]interface{}{}
+// 	err := r.GetPayload(&payload)
+// 	tSession := new(acl.Session)
+// 	err = acl.FindByID(tSession, payload["_id"].(string))
+// 	if err != nil {
+// 		return helper.CreateResult(true, nil, err.Error())
+// 	} else {
+// 		return helper.CreateResult(true, tSession, "")
+// 	}
 
-	if err != nil {
-		return helper.CreateResult(true, nil, err.Error())
-	}
+// }
+// func (a *SessionController) DeleteSession(r *knot.WebContext) interface{} {
+// 	r.Config.OutputType = knot.OutputJson
+// 	a.InitialSetDatabase()
+// 	tSession := new(acl.Session)
+// 	payload := map[string]interface{}{}
+// 	err := r.GetPayload(&payload)
+// 	err = acl.FindByID(tSession, payload["_id"].(string))
 
-	err = acl.Delete(tSession)
-	if err != nil {
-		return helper.CreateResult(true, nil, err.Error())
-	}
+// 	if err != nil {
+// 		return helper.CreateResult(true, nil, err.Error())
+// 	}
 
-	return helper.CreateResult(true, nil, "success")
-}
+// 	err = acl.Delete(tSession)
+// 	if err != nil {
+// 		return helper.CreateResult(true, nil, err.Error())
+// 	}
+
+// 	return helper.CreateResult(true, nil, "success")
+// }
 
 func (a *SessionController) prepareconnection() (conn dbox.IConnection, err error) {
 	conn, err = dbox.NewConnection("mongo",
