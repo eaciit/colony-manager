0f6da6f9c28b942d294b7463f43291a0e5ad9e6b
Arfian Bagus <arfianbagus@Arfians-MacBook-Pro.local>
2016-03-22 17:00:39 +0700

    top menu widget

--- controller/web.go
+++ controller/web.go
@@ -77,12 +77,20 @@ func (w *WebController) FileBrowser(r *knot.WebContext) interface{} {
 	return true
 }
 
-
 func (w *WebController) Administration(r *knot.WebContext) interface{} {
 	r.Config.OutputType = knot.OutputTemplate
 	r.Config.LayoutTemplate = LayoutFile
 	r.Config.IncludeFiles = IncludeFiles
 	r.Config.ViewName = "views/page-administration.html"
+
+	return true
+}
+
+func (w *WebController) WidgetGrid(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputTemplate
+	r.Config.LayoutTemplate = LayoutFile
+	r.Config.IncludeFiles = IncludeFiles
+	r.Config.ViewName = "views/page-widgetgrid.html"
 
 	return true
 }
