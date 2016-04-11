8c1a98eaefaee9960a740b54abe65bd5fb07275c
Arfian Bagus <arfianbagus@Arfians-MacBook-Pro.local>
2016-04-11 09:59:17 +0700

    no message

--- assets/widget/widgetdesigner.js
+++ assets/widget/widgetdesigner.js
@@ -1,18 +1,82 @@
-$(function () {
-	var options = {
-        width: 12,
-        float: false,
-        removable: false,
-        // acceptWidgets: '.grid-stack-item',
-        acceptWidgets: '.grid-stack-item',
-        // placeholderText: 'Add Widget In Here !',
-        // appendTo: 'body'
+var widgets = [
+        {x: 0, y: 0, width: 2, height: 2},
+        {x: 2, y: 0, width: 4, height: 2},
+        {x: 6, y: 0, width: 2, height: 4},
+        {x: 1, y: 2, width: 4, height: 2}
+    ];
+var widgetFunc = function (widgets) {
+    var self = this;
+
+    this.widgets = ko.observableArray(widgets);
+
+    this.addNewWidget = function () {
+        this.widgets.push({
+            x: 0,
+            y: 0,
+            width: 4,
+            height: 4,
+            auto_position: true
+        });
+        return false;
     };
-    $('#panel-designer').gridstack(options);
+
+    this.deleteWidget = function (item) {
+        self.widgets.remove(item);
+        return false;
+    };
+};
+pg.widgetgrid = new widgetFunc(widgets);
+
+ko.components.register('widget-grid', {
+    viewModel: {
+        createViewModel: function (controller, componentInfo) {
+            var ViewModel = function (controller, componentInfo) {
+                var grid = null;
+                this.widgets = controller.widgetgrid.widgets;
+
+                this.afterAddWidget = function (items) {
+                    if (grid == null) {
+                        grid = $(componentInfo.element).find('.grid-stack').gridstack({
+                            auto: false,
+                            acceptWidgets: '.grid-stack-item',
+                        }).data('gridstack');
+                    }
+
+                    var item = _.find(items, function (i) { return i.nodeType == 1 });
+                    grid.addWidget(item);
+                    ko.utils.domNodeDisposal.addDisposeCallback(item, function () {
+                        grid.removeWidget(item);
+                    });
+                };
+            };
+            return new ViewModel(controller, componentInfo);
+        }
+    },
+    template:
+        [
+            '<div class="grid-stack" data-bind="foreach: {data: widgets, afterRender: afterAddWidget}">',
+            '   <div class="grid-stack-item" data-bind="attr: {\'data-gs-x\': $data.x, \'data-gs-y\': $data.y, \'data-gs-width\': $data.width, \'data-gs-height\': $data.height, \'data-gs-auto-position\': $data.auto_position}">',
+            '       <div class="grid-stack-item-content"><button data-bind="click: $root.deleteWidget">Delete me</button></div>',
+            '   </div>',
+            '</div> '
+        ].join('')
+});
+
+$(function () {
+	// var options = {
+ //        width: 12,
+ //        float: false,
+ //        removable: false,
+ //        acceptWidgets: '.grid-stack-item',
+ //        placeholderText: 'Add Widget In Here !',
+ //        appendTo: 'body'
+ //    };
+    // $('#panel-designer').gridstack(options);
 	$('#sidebar .grid-stack-item').draggable({
 	    handle: '.grid-stack-item-content',
-	    scroll: false,
-	    // appendTo: 'body',
+        helper: "clone",
+	    scroll: true,
+	    appendTo: 'body',
 	  //   stop: function(event, ui) {
 
 			// $('#panel-designer').data("gridstack").addWidget($('<div class="grid-stack-item-content"> Example Widget </div>'), 0, 0);
