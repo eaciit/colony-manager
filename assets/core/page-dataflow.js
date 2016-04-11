b8cd2a1b6b79edeab81dd6db247b4a682329f025
yanda15 <yanda151092@gmail.com>
2016-04-11 14:43:07 +0700

    data flow

--- assets/core/page-dataflow.js
+++ assets/core/page-dataflow.js
@@ -407,44 +407,37 @@ df.init = function () {
                             $(".arrow").attr("style","left:30px"); 
                         }
 
+                        $(".popover-title").html(item.dataItem.name+" - "+$(".diagram").getKendoDiagram().select()[0].id);
+
                         setTimeout(function () {
                             ko.cleanNode($(".popover-content:last")[0]);
                             ko.applyBindings(viewModel, $(".popover-content:last")[0]);
                         }, 10);
                         if (item.dataItem.name == "Spark") {
-                            $(".popover-title").html(item.dataItem.name);
                             $(".popover").attr("style","display: block; top: " +(ymouse-220)+"px; left: "+(xmouse-30)+"px;");
                             df.popoverMode('spark');
                         }else if(item.dataItem.name == "HDFS"){
-                            $(".popover-title").html(item.dataItem.name);
                             $(".popover").attr("style","display: block; top: " +(ymouse-120)+"px; left: "+(xmouse-30)+"px;");
                             df.popoverMode('hdfs');                            
                         }else if(item.dataItem.name == "Hive"){
-                            $(".popover-title").html(item.dataItem.name);
                             $(".popover").attr("style","display: block; top: " +(ymouse-150)+"px; left: "+(xmouse-30)+"px;");
                             df.popoverMode('hive');
                         }else if(item.dataItem.name == "Shell Script"){
-                            $(".popover-title").html(item.dataItem.name);
                             $(".popover").attr("style","display: block; top: " +(ymouse-120)+"px; left: "+(xmouse-30)+"px;");
                             df.popoverMode('shell');
                         }else if(item.dataItem.name == "SSH Script"){
-                            $(".popover-title").html(item.dataItem.name);
                             $(".popover").attr("style","display: block; top: " +(ymouse-120)+"px; left: "+(xmouse-30)+"px;");
                             df.popoverMode('ssh');
                         }else if(item.dataItem.name == "Map Reduce"){
-                            $(".popover-title").html(item.dataItem.name);
                             $(".popover").attr("style","display: block; top: " +(ymouse-150)+"px; left: "+(xmouse-30)+"px;");
                             df.popoverMode('mapreduce');
                         }else if(item.dataItem.name == "Java App"){
-                            $(".popover-title").html(item.dataItem.name);
                             $(".popover").attr("style","display: block; top: " +(ymouse-150)+"px; left: "+(xmouse-30)+"px;");
                             df.popoverMode('java');
                         }else if(item.dataItem.name == "Email"){
-                            $(".popover-title").html(item.dataItem.name);
                             $(".popover").attr("style","display: block; top: " +(ymouse-210)+"px; left: "+(xmouse-30)+"px;");
                             df.popoverMode('email');
                         }else if(item.dataItem.name == "Stop"){
-                            $(".popover-title").html(item.dataItem.name);
                             $(".popover").attr("style","display: block; top: " +(ymouse-210)+"px; left: "+(xmouse-30)+"px;");
                             df.popoverMode('stop');
                         }else if(item.dataItem.name == "Fork"){
@@ -454,7 +447,6 @@ df.init = function () {
                                 df.popoverMode('fork');
                                 // console.log(no);
                                 if(no !== 0){
-                                    $(".popover-title").html(item.dataItem.name);
                                     $("#fork").append($("#fork-row").html());
                                     $(".popover").attr("style","display: block; top: " +(ymouse-100)+"px; left: "+(xmouse-30)+"px;");
                                 };
