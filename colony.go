9c135d3ce59eb1f17655db5fb6964523008ac1b6
novalagung <caknopal@gmail.com>
2016-04-14 15:44:28 +0700

    no message

--- colony.go
+++ colony.go
@@ -36,13 +36,13 @@ func main() {
 	runtime.GOMAXPROCS(4)
 	colonycore.ConfigPath = filepath.Join(controller.EC_APP_PATH, "config")
 
-	pageController := controller.CreatePageController(server)
+	webController := controller.CreateWebController(server)
 
 	server = new(knot.Server)
 	server.Address = "localhost:3000"
 	server.RouteStatic("res", path.Join(controller.AppBasePath, "assets"))
 	server.RouteStatic("res-widget", path.Join(controller.EC_DATA_PATH, "widget"))
-	server.Register(controller.CreateWebController(server), "")
+	server.Register(webController, "")
 	server.Register(controller.CreateDataBrowserController(server), "")
 	server.Register(controller.CreateDataSourceController(server), "")
 	server.Register(controller.CreateDataGrabberController(server), "")
@@ -58,7 +58,7 @@ func main() {
 	server.Register(controller.CreateAclController(server), "")
 	server.Register(controller.CreateSessionController(server), "")
 	server.Register(controller.CreateWidgetController(server), "")
-	server.Register(pageController, "")
+	server.Register(controller.CreatePageController(server), "")
 	server.Register(controller.CreateLoginController(server), "")
 
 	if colonycore.GetConfig(colonycore.CONF_DB_ACL) == nil || isSetupACL {
@@ -80,7 +80,7 @@ func main() {
 		rURL := r.Request.URL.String()
 		if regex.MatchString(rURL) {
 			args := strings.Split(strings.Replace(rURL, "/page/", "", -1), "/")
-			return pageController.View(r, args)
+			return webController.PageView(r, args)
 		}
 
 		// is dev mode?
