145e726eea9af1bce870fde4d02818eff85d8567
frezadev <freza.dev@gmail.com>
2016-04-15 15:20:04 +0700

    merging

--- controller/data-flow/dataflowapi.go
+++ controller/data-flow/dataflowapi.go
@@ -3,16 +3,19 @@ package dataflow
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	//"math/rand"
 	"github.com/eaciit/colony-core/v0"
+	"github.com/eaciit/dbox"
 	//"github.com/eaciit/dbox"
 	"github.com/eaciit/hdc/hdfs"
 	"github.com/eaciit/hdc/hive"
 	//"github.com/eaciit/sshclient"
-	"github.com/eaciit/toolkit"
 	"encoding/json"
 	"strings"
 	"time"
+
+	"github.com/eaciit/toolkit"
 )
 
 const (
@@ -23,6 +26,12 @@ const (
 	ACTION_TYPE_SSH      = "SSH"
 	ACTION_TYPE_KAFKA    = "KAFKA"
 
+	ACTION_TYPE_MAP_REDUCE = "MR"
+	ACTION_TYPE_EMAIL      = "EMAIL"
+	ACTION_TYPE_STOP       = "STOP"
+	ACTION_TYPE_FORK       = "FORK"
+	ACTION_TYPE_JAVA       = "JAVA"
+
 	FORK_TYPE_ALL       = "ALL"
 	FORK_TYPE_ONE       = "ONE"
 	FORK_TYPE_MANDATORY = "MANDATORY"
@@ -30,11 +39,10 @@ const (
 	SSH_OPERATION_MKDIR = "MKDIR"
 	// SSH_OPERATION_* please define
 
-
 	ACT_RESULT_PATH = "/usr/eaciit/dataflow/result/"
-	CMD_SPARK      = "spark-submit %v"
-	CMD_MAP_REDUCE = "hadoop jar %v -input %v -output %v -mapper %v -reducer %v"
-	CMD_JAVA       = "java -jar %v"
+	CMD_SPARK       = "spark-submit %v"
+	CMD_MAP_REDUCE  = "hadoop jar %v -input %v -output %v -mapper %v -reducer %v"
+	CMD_JAVA        = "java -jar %v"
 )
 
 //var CurrentAction colonycore.FlowAction
@@ -44,25 +52,25 @@ var act_result_path string
 
 // Start, to start the flow process
 func Start(flow colonycore.DataFlow, user string, globalParam toolkit.M) (processID string, e error) {
-	//var steps []colonycore.FlowAction
+	var steps []colonycore.FlowAction
 	// steps = append(steps, flow.Actions[0].(colonycore.FlowAction))
 
-	/*process := colonycore.DataFlowProcess{
-		Id:          generateProcessID(),
+	process := colonycore.DataFlowProcess{
+		Id:          generateProcessID(flow.ID),
 		Flow:        flow,
 		StartDate:   time.Now(),
 		StartedBy:   user,
 		Steps:       steps,
 		GlobalParam: globalParam,
-	}*/
+	}
 
-	//// save the DataFlowProcess
-	//err = colonycore.Save(process)
-	/*if e != nil {
+	// save the DataFlowProcess
+	e = colonycore.Save(&process)
+	if e != nil {
 		return
 	}
 
-	processID = process.Id*/
+	processID = process.Id
 
 	// run the watcher and run the process, please update regardingly
 	go watch(flow)
@@ -78,57 +86,67 @@ func Start(flow colonycore.DataFlow, user string, globalParam toolkit.M) (proces
 
 // runProcess, process the flow
 func runProcess(process colonycore.DataFlow, action colonycore.FlowAction, actionBefore []colonycore.FlowAction) (e error) {
+	var res []toolkit.M
 	switch action.Type {
-
-		case ACTION_TYPE_HIVE:
-			e = runHive(process, action)
-			break
-		case ACTION_TYPE_HDFS:
-			e = runHDFS(process, action)
-			break
-		case ACTION_TYPE_SPARK:
-			e = runSpark(process, action)
-			break
-		case ACTION_TYPE_SSH:
-			e = runSSH(process, action)
-			break
-
+	case ACTION_TYPE_HIVE:
+		res, e = runHive(process, action)
+		break
+	case ACTION_TYPE_HDFS:
+		res, e = runHDFS(process, action)
+		break
+	case ACTION_TYPE_SPARK:
+		res, e = runSpark(process, action)
+		break
+	case ACTION_TYPE_SSH:
+		res, e = runSSH(process, action)
+		break
+	case ACTION_TYPE_EMAIL:
+		// res, e = runSSH(process, action)
+		break
+	case ACTION_TYPE_MAP_REDUCE:
+		res, e = runMapReduce(process, action)
+		break
+	case ACTION_TYPE_JAVA:
+		res, e = runJava(process, action)
+		break
 	}
+	fmt.Println(res)
 	var msg string
-	if e != nil { msg = "status : NOK \n Error : " + e.Error() } else { msg = "status : OK \n Error : " }
+	if e != nil {
+		msg = "status : NOK \n Error : " + e.Error()
+	} else {
+		msg = "status : OK \n Error : "
+	}
 	writeActionResultStatus(process, action, msg)
 	return e
 }
 
-func runHive(process colonycore.DataFlow, action colonycore.FlowAction) (e error) {
-	var action_hive colonycore.ActionHive
-	action_hive = action.Action.(colonycore.ActionHive)
+func runHive(process colonycore.DataFlow, action colonycore.FlowAction) (res []toolkit.M, e error) {
+	action_hive := action.Action.(colonycore.ActionHive)
 	hivex = hive.HiveConfig(action.Server.Host, "", "Username", "Password", "Path")
 
-	var res []toolkit.M
 	e = hivex.Populate(action_hive.ScriptPath, &res)
 
-	return
+	return res, e
 }
 
-func runHDFS(process colonycore.DataFlow, action colonycore.FlowAction) (e error) {
-	var res toolkit.M
+func runHDFS(process colonycore.DataFlow, action colonycore.FlowAction) (res []toolkit.M, e error) {
 	server := action.Server
 	setting, _, e := (&server).Connect()
 	hdfs := action.Action.(colonycore.ActionHDFS)
+
 	result, e := setting.GetOutputCommandSsh(hdfs.Command)
 
 	if e != nil {
-		return e
+		return res, e
 	}
 
-	res.Set("result_stdout", result)
+	res = append(res, toolkit.M{}.Set("result_stdout", result))
 	return
 }
 
-func runSpark(process colonycore.DataFlow, action colonycore.FlowAction) (e error) {
-	var spark colonycore.ActionSpark
-	spark = action.Action.(colonycore.ActionSpark)
+func runSpark(process colonycore.DataFlow, action colonycore.FlowAction) (res []toolkit.M, e error) {
+	spark := action.Action.(colonycore.ActionSpark)
 	args := ""
 
 	if spark.MainClass != "" {
@@ -138,8 +156,6 @@ func runSpark(process colonycore.DataFlow, action colonycore.FlowAction) (e erro
 	if spark.Master != "" {
 		args = args + " --master " + spark.Master
 	}
-
-	if  e != nil { return e }
 
 	if spark.Mode != "" {
 		args = args + " --deploy-mode " + spark.Mode
@@ -152,54 +168,62 @@ func runSpark(process colonycore.DataFlow, action colonycore.FlowAction) (e erro
 	if spark.Args != "" {
 		args = args + " " + spark.Args
 	}
-	return 
-}
 
-func runSSH(process colonycore.DataFlow, action colonycore.FlowAction) (e error) {
+	cmd := fmt.Sprintf(CMD_SPARK, args)
+
 	server := action.Server
 	setting, _, e := (&server).Connect()
-	action_ssh := action.Action.(colonycore.ActionSSH)
-
-	var res toolkit.M
-	result, e := setting.GetOutputCommandSsh(action_ssh.Command)
+	result, e := setting.GetOutputCommandSsh(cmd)
 
 	if e != nil {
-		return e
+		return res, e
 	}
 
-	res.Set("result_stdout", result)
+	res = append(res, toolkit.M{}.Set("result_stdout", result))
 	return
 }
 
-func runShell(process colonycore.DataFlow, action colonycore.FlowAction) (e error) {
-	var res toolkit.M
+func runSSH(process colonycore.DataFlow, action colonycore.FlowAction) (res []toolkit.M, e error) {
+	server := action.Server
+	setting, _, e := (&server).Connect()
+	ssh := action.Action.(colonycore.ActionSSH)
+
+	result, e := setting.GetOutputCommandSsh(ssh.Command)
+
+	if e != nil {
+		return res, e
+	}
+
+	res = append(res, toolkit.M{}.Set("result_stdout", result))
+	return
+}
+
+func runShell(process colonycore.DataFlow, action colonycore.FlowAction) (res []toolkit.M, e error) {
 	server := action.Server
 	setting, _, e := (&server).Connect()
 	shell := action.Action.(colonycore.ActionShellScript)
-
 	result, e := setting.GetOutputCommandSsh(shell.Script)
 
 	if e != nil {
-		return e
+		return res, e
 	}
 
-	res.Set("result_stdout", result)
+	res = append(res, toolkit.M{}.Set("result_stdout", result))
 	return
 }
 
-func runMapReduce(process colonycore.DataFlow, action colonycore.FlowAction) (e error) {
-	/*var res []toolkit.M
+func runMapReduce(process colonycore.DataFlow, action colonycore.FlowAction) (res []toolkit.M, e error) {
 	server := action.Server
 	setting, _, e := (&server).Connect()
-	mr := action.Action.(colonycore.ActionShellScript)*/
+	mr := action.Action.(colonycore.ActionHadoopStreaming)
 
-	//cmd := fmt.Sprintf(CMD_MAP_REDUCE, mr.Jar, mr.Input, mr.Output, mr.Mapper, mr.Reducer)
+	cmd := fmt.Sprintf(CMD_MAP_REDUCE, mr.Jar, mr.Input, mr.Output, mr.Mapper, mr.Reducer)
 
-	// for _, file := range mr.Files {
-	// 	if file != "" {
-	// 		cmd = cmd + " -file" + file
-	// 	}
-	// }
+	for _, file := range mr.Files {
+		if file != "" {
+			cmd = cmd + " -file" + file
+		}
+	}
 
 	/*for _, param := range mr.Params {
 		if param != "" {
@@ -207,34 +231,32 @@ func runMapReduce(process colonycore.DataFlow, action colonycore.FlowAction) (e 
 		}
 	}*/
 
-	// if mr.Params != "" {
-	// 	cmd = cmd + " " + mr.Params
-	// }
-
-	//result, e := sshclient.GetOutputCommandSsh(cmd)
-
-	if e != nil {
-		return e
+	if mr.Params != "" {
+		cmd = cmd + " " + mr.Params
 	}
-
-	// res.Set("result_stdout", result)
-	return
-}
-
-func runJava(process colonycore.DataFlow, action colonycore.FlowAction) (e error) {
-	var res toolkit.M
-	server := action.Server
-	setting, _, e := (&server).Connect()
-	java := action.Action.(colonycore.ActionJavaApp)
-	cmd := fmt.Sprintf(CMD_JAVA, java.Jar)
 
 	result, e := setting.GetOutputCommandSsh(cmd)
 
 	if e != nil {
-		return e
+		return res, e
 	}
 
-	res.Set("result_stdout", result)
+	res = append(res, toolkit.M{}.Set("result_stdout", result))
+	return
+}
+
+func runJava(process colonycore.DataFlow, action colonycore.FlowAction) (res []toolkit.M, e error) {
+	server := action.Server
+	setting, _, e := (&server).Connect()
+	java := action.Action.(colonycore.ActionJavaApp)
+	cmd := fmt.Sprintf(CMD_JAVA, java.Jar)
+	result, e := setting.GetOutputCommandSsh(cmd)
+
+	if e != nil {
+		return res, e
+	}
+
+	res = append(res, toolkit.M{}.Set("result_stdout", result))
 	return
 }
 
@@ -245,7 +267,7 @@ func watch(process colonycore.DataFlow) (e error) {
 	var ListCurrentTierAction, ListLastTierAction, ListNextTierAction []colonycore.FlowAction
 
 	//get initial current tier action
-	for _, action := range process.Actions{
+	for _, action := range process.Actions {
 		if action.FirstAction {
 			ListCurrentTierAction = append(ListCurrentTierAction, action)
 		}
@@ -261,19 +283,22 @@ func watch(process colonycore.DataFlow) (e error) {
 			var ActionBefore []colonycore.FlowAction
 
 			//check previous action result status file to make sure that previous action was complete
-			for isFound == false  {
+			for isFound == false {
 				ActionBefore = getActionBefore(ListLastTierAction, CurrentAction)
 
 				if ActionBefore != nil {
-					for _, actx := range ActionBefore{
+					for _, actx := range ActionBefore {
 						files, e = getActionResultStatus(process, actx)
-						if e!= nil { return ; break}
+						if e != nil {
+							return
+							break
+						}
 						if files != nil {
-							isFound = true 
-							filenames = append(filenames,getActionResultStatusPath(process, actx))
+							isFound = true
+							filenames = append(filenames, getActionResultStatusPath(process, actx))
 						} else {
-							isFound = false 
-							filenames = []string{} 
+							isFound = false
+							filenames = []string{}
 							break
 						}
 					}
@@ -288,7 +313,7 @@ func watch(process colonycore.DataFlow) (e error) {
 				for _, fname := range filenames {
 					files, e := ioutil.ReadFile(fname)
 
-					if e!= nil{
+					if e != nil {
 						return e
 					}
 
@@ -297,27 +322,27 @@ func watch(process colonycore.DataFlow) (e error) {
 							ListLastTierAction = append(ListLastTierAction, CurrentAction)
 							nextIdx = append(nextIdx, GetAction(process, ok).Id)
 						}
-					}else {
+					} else {
 						for _, nok := range CurrentAction.KO {
 							ListLastTierAction = append(ListLastTierAction, CurrentAction)
 							nextIdx = append(nextIdx, GetAction(process, nok).Id)
 						}
 					}
 
-					go runProcess(process, CurrentAction, ActionBefore) 
+					go runProcess(process, CurrentAction, ActionBefore)
 				}
 			}
 
 			//get next tier action
 			if len(nextIdx) > 0 {
-				for _ , idx := range nextIdx{
+				for _, idx := range nextIdx {
 					ListNextTierAction = append(ListNextTierAction, GetAction(process, idx))
 				}
 			}
 		}
 
 		ListLastTierAction = []colonycore.FlowAction{}
-		for _, curr := range ListCurrentTierAction{
+		for _, curr := range ListCurrentTierAction {
 			ListLastTierAction = append(ListLastTierAction, curr)
 		}
 
@@ -333,6 +358,18 @@ func watch(process colonycore.DataFlow) (e error) {
 }
 
 //  generateProcessID generate the ID
+func generateProcessID(flowID string) (processID string) {
+	now := time.Now()
+	y := strconv.Itoa(now.Year())
+	m := strconv.Itoa(int(now.Month()))
+	d := strconv.Itoa(now.Day())
+	hh := strconv.Itoa(now.Hour())
+	nm := strconv.Itoa(now.Minute())
+	ns := strconv.Itoa(now.Second())
+	processID = flowID + "-" + y + m + d + hh + nm + ns
+	return
+}
+
 /*func generateProcessID(flowID string) (processID string) {
 	processID = flowID + "-" + randString(10)
 	return
@@ -359,7 +396,7 @@ func randString(n int) string {
 }*/
 
 // GetStatus get the status of running flow
-/*func GetProcessedFlow(id string) (process colonycore.DataFlowProcess, e error) {
+func GetProcessedFlow(id string) (process colonycore.DataFlowProcess, e error) {
 	dataDs := []colonycore.DataFlowProcess{}
 	cursor, e := colonycore.Find(new(colonycore.DataFlowProcess), dbox.Eq("_id", id))
 	if cursor != nil {
@@ -368,7 +405,7 @@ func randString(n int) string {
 	}
 
 	if e != nil && cursor != nil {
-		return nil, e
+		return
 	}
 
 	if len(dataDs) > 0 {
@@ -377,7 +414,7 @@ func randString(n int) string {
 
 	return
 }
-*/
+
 func getActionResultStatusPath(flow colonycore.DataFlow, action colonycore.FlowAction) (dirName string) {
 	return act_result_path + "/" + action.Id + "/act_status/res_Status.txt"
 }
@@ -392,8 +429,8 @@ func GetAction(flow colonycore.DataFlow, actionId string) (action colonycore.Flo
 	return
 }
 
-func getActionBefore(ListActionBefore []colonycore.FlowAction, CurrentAction colonycore.FlowAction)(ActionBefore []colonycore.FlowAction){
-	for _, act := range ListActionBefore{
+func getActionBefore(ListActionBefore []colonycore.FlowAction, CurrentAction colonycore.FlowAction) (ActionBefore []colonycore.FlowAction) {
+	for _, act := range ListActionBefore {
 		for _, idx := range act.OK {
 			if idx == CurrentAction.Id {
 				ActionBefore = append(ActionBefore, act)
@@ -409,23 +446,23 @@ func getActionBefore(ListActionBefore []colonycore.FlowAction, CurrentAction col
 	return
 }
 
-func setCommandArgument(flow colonycore.DataFlow, action colonycore.FlowAction, ActionBefore []colonycore.FlowAction)(arguments []string){
+func setCommandArgument(flow colonycore.DataFlow, action colonycore.FlowAction, ActionBefore []colonycore.FlowAction) (arguments []string) {
 	return nil
 }
 
-func writeActionResultStatus(flow colonycore.DataFlow, action colonycore.FlowAction, msg string)(err error){
+func writeActionResultStatus(flow colonycore.DataFlow, action colonycore.FlowAction, msg string) (err error) {
 	filepath := act_result_path + "/" + action.Id + "/act_status/res_Status.txt"
 	err = ioutil.WriteFile(filepath, []byte(msg), 0755)
 	return
 }
 
-func getActionResultStatus(flow colonycore.DataFlow, action colonycore.FlowAction)(file []byte, err error){
+func getActionResultStatus(flow colonycore.DataFlow, action colonycore.FlowAction) (file []byte, err error) {
 	temppath := act_result_path + "/" + action.Id + "/act_status/res_Status.txt"
 	res, err := ioutil.ReadFile(temppath)
 	return res, err
 }
 
-func decodeOutputFile(action colonycore.FlowAction)(output interface{}, e error){
+func decodeOutputFile(action colonycore.FlowAction) (output interface{}, e error) {
 	outputPath := action.OutputPath
 
 	file, e := ioutil.ReadFile(outputPath)
@@ -434,28 +471,32 @@ func decodeOutputFile(action colonycore.FlowAction)(output interface{}, e error)
 	}
 
 	switch action.OutputType {
-		case "JSON":
-			e = json.Unmarshal(file, &output)
-		case "CSV":break
-			output, e = decodeCSV(file)
-		case "TSV":break
-			output, e = decodeTSV(file)
-		case "TEXT":break
-			output, e = decodeText(file)
-		default:break
+	case "JSON":
+		e = json.Unmarshal(file, &output)
+	case "CSV":
+		break
+		output, e = decodeCSV(file)
+	case "TSV":
+		break
+		output, e = decodeTSV(file)
+	case "TEXT":
+		break
+		output, e = decodeText(file)
+	default:
+		break
 	}
 
-	return 
-}
-
-func decodeCSV(file []byte)(retVal interface{}, e error){
 	return
 }
 
-func decodeTSV(file []byte)(retVal interface{}, e error){
+func decodeCSV(file []byte) (retVal interface{}, e error) {
 	return
 }
 
-func decodeText(file []byte)(retVal interface{}, e error){
+func decodeTSV(file []byte) (retVal interface{}, e error) {
+	return
+}
+
+func decodeText(file []byte) (retVal interface{}, e error) {
 	return
 }
