acc5fe9d9d0e7f4c2fb0e016a093ee778500a47f
frezadev <freza.dev@gmail.com>
2016-04-18 13:36:07 +0700

    constructAction changes

--- controller/dataflow.go
+++ controller/dataflow.go
@@ -43,10 +43,11 @@ func (a *DataFlowController) Save(r *knot.WebContext) interface{} {
 	}
 
 	dataShapes := payload["DataShapes"].(map[string]interface{})
+	actions := payload["Actions"].([]interface{})
 
 	currentDataFlow := new(colonycore.DataFlow)
 	currentDataFlow.DataShapes = dataShapes
-	currentDataFlow.Actions = constructActions(dataShapes)
+	currentDataFlow.Actions = constructActions(actions)
 	currentDataFlow.Name = tk.ToString(payload["Name"])
 	currentDataFlow.Description = tk.ToString(payload["Description"])
 	currentDataFlow.ID = tk.ToString(payload["ID"])
@@ -81,7 +82,157 @@ func (a *DataFlowController) Save(r *knot.WebContext) interface{} {
 	return helper.CreateResult(true, nil, "success")
 }
 
-func constructActions(dataShapes map[string]interface{}) (actions []colonycore.FlowAction) {
+func constructActions(list []interface{}) (flows []colonycore.FlowAction) {
+	for _, act := range list {
+		flowAction := act.(map[string]interface{})
+		server := colonycore.Server{}
+		if flowAction["server"] != nil {
+			dsServer := []colonycore.Server{}
+			cursor, err := colonycore.Find(new(colonycore.Server), dbox.Eq("_id", flowAction["server"].(string)))
+
+			if err != nil {
+				//
+			} else {
+				if cursor != nil {
+					cursor.Fetch(&dsServer, 0, false)
+					defer cursor.Close()
+				}
+
+				if len(dsServer) > 0 {
+					server = dsServer[0]
+				}
+			}
+		}
+
+		inputParam := tk.M{}
+		outputParam := tk.M{}
+		outputType := ""
+
+		for _, in := range flowAction["inputparam"].([]interface{}) {
+			inputParam.Set("", in)
+		}
+
+		if flowAction["outputtype"] != nil {
+			outputType = flowAction["outputtype"].(string)
+		}
+
+		for _, out := range flowAction["outputparam"].([]interface{}) {
+			outputParam.Set("", out)
+		}
+
+		OK := []string{}
+		KO := []string{}
+
+		for _, str := range flowAction["OK"].([]interface{}) {
+			OK = append(OK, str.(string))
+		}
+
+		for _, str := range flowAction["KO"].([]interface{}) {
+			KO = append(KO, str.(string))
+		}
+
+		actionType := flowAction["type"].(string)
+
+		flow := colonycore.FlowAction{
+			Id:          flowAction["id"].(string),
+			Name:        flowAction["name"].(string) + "-" + flowAction["id"].(string),
+			Description: flowAction["description"].(string),
+			Type:        actionType,
+			Server:      server,
+			OK:          OK,
+			KO:          KO,
+			Retry:       tk.ToInt(flowAction["Retry"], ""),
+			Interval:    tk.ToInt(flowAction["Interval"], ""),
+			FirstAction: flowAction["firstaction"].(bool),
+			InputParam:  inputParam,
+			OutputParam: outputParam,
+			OutputType:  outputType,
+			OutputPath:  "",
+		}
+
+		dataAction := flowAction["action"].(map[string]interface{})
+
+		switch actionType {
+		case dataflow.ACTION_TYPE_SPARK:
+			spark := colonycore.ActionSpark{
+				Master:    dataAction["master"].(string),
+				Mode:      dataAction["mode"].(string),
+				File:      dataAction["appfiles"].(string),
+				MainClass: dataAction["mainclass"].(string),
+				// Args:      dataAction["args"].(string),
+			}
+			flow.Action = spark
+			break
+		case dataflow.ACTION_TYPE_HIVE:
+			hive := colonycore.ActionHive{
+				ScriptPath: dataAction["scriptpath"].(string),
+				// Params:     ,
+			}
+			flow.Action = hive
+			break
+		case dataflow.ACTION_TYPE_SSH:
+			ssh := colonycore.ActionSSH{
+				Command: dataAction["script"].(string),
+			}
+			flow.Action = ssh
+			break
+		case dataflow.ACTION_TYPE_HDFS:
+			hdfs := colonycore.ActionHDFS{
+			// Command: dataAction["script"].(string),
+			}
+			flow.Action = hdfs
+			break
+		case dataflow.ACTION_TYPE_JAVA:
+			java := colonycore.ActionJavaApp{
+				Jar: dataAction["jar"].(string),
+			}
+			flow.Action = java
+			break
+		case dataflow.ACTION_TYPE_MAP_REDUCE:
+			mr := colonycore.ActionHadoopStreaming{
+				// Jar: ,
+				Mapper:  dataAction["mapper"].(string),
+				Reducer: dataAction["reducer"].(string),
+				Input:   dataAction["input"].(string),
+				Output:  dataAction["output"].(string),
+			}
+			flow.Action = mr
+			break
+		case dataflow.ACTION_TYPE_KAFKA:
+			kafka := colonycore.ActionKafka{}
+			flow.Action = kafka
+			break
+		case dataflow.ACTION_TYPE_EMAIL:
+			email := colonycore.ActionEmail{
+			// To:,
+			//   CC:,
+			//   Subject:,
+			//   Body:,
+			}
+			flow.Action = email
+			break
+		// case "Fork":
+		// 	fork := colonycore.ActionFork{}
+		// 	action.Action = fork
+		// 	action.Type = dataflow.ACTION_TYPE_FORK
+		// 	break
+		case dataflow.ACTION_TYPE_DECISION:
+			decision := colonycore.ActionDecision{}
+			flow.Action = decision
+			break
+		case dataflow.ACTION_TYPE_STOP:
+			stop := colonycore.ActionStop{}
+			flow.Action = stop
+			break
+		}
+
+		flows = append(flows, flow)
+	}
+
+	return
+}
+
+func constructActionsX(dataShapes map[string]interface{}) (actions []colonycore.FlowAction) {
 	shapes := dataShapes["shapes"]
 	connections := dataShapes["connections"].([]interface{})
 
@@ -220,10 +371,10 @@ func constructActions(dataShapes map[string]interface{}) (actions []colonycore.F
 				break
 			case "Email":
 				email := colonycore.ActionEmail{
-				/*To:,
-				  CC:,
-				  Subject:,
-				  Body:,*/
+				// To:,
+				//   CC:,
+				//   Subject:,
+				//   Body:,
 				}
 				action.Action = email
 				action.Type = dataflow.ACTION_TYPE_EMAIL
