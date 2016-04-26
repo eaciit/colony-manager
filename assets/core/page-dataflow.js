239c1fd92583515109c33e3642dbe7433fb0a70c
Adinda Martha <ndte913@gmail.com>
2016-04-26 16:47:29 +0700

    no message

--- assets/core/page-dataflow.js
+++ assets/core/page-dataflow.js
@@ -1033,6 +1033,7 @@ df.createGrid = function(){
                                 datas[i].dfdescription = datas[i].description;
                             }
                             res.data.data = datas;
+                            dfl(res.data.data);
                             yo.success(res.data);
                         }
                     });
@@ -1072,15 +1073,9 @@ df.createGrid = function(){
                 },
             },
             {field:"createdby",width:200,title:"Created By"},
-            {title: "Actions", width: 130, attributes:{class:"align-center"}, headerAttributes: {style: "text-align: center;"},template: function(d){
-                return[
-                    "<div class='btn-group btn-sm'><button class='btn btn-sm btn-default btn-primary tooltipster-grid' title='Design' onclick='df.goToDesigner(\"#:_id#\")'><span class='glyphicon glyphicon-cog'></span></button>",
-                    "<button class='btn btn-sm btn-default btn-primary tooltipster-grid' title='Delete' onclick='df.delete(\"#:_id#\")'><span class='glyphicon glyphicon-trash'></span</button></div>",
-                ].join(" ");
-            }}
-            // {width:100, title:"Actions",
-            //     template:"<button class='btn btn-sm tooltipster-grid mgRight10' title='design' onclick='df.goToDesigner(\"#:_id#\")' ><span class='glyphicon glyphicon-cog'></span></button>"+
-            // "<button class='btn btn-sm tooltipster-grid' title='delete' onclick='df.delete(\"#:_id#\")' ><span class='glyphicon glyphicon-trash'></span></button>"},
+            {width:130, headerAttributes: {style: "text-align: center;"}, attributes:{class:"align-center"}, title:"Actions",
+                template:"<div class='btn-group btn-sm'><button class='btn btn-sm btn-default btn-primary tooltipster-grid' title='design' onclick='df.goToDesigner(\"#:_id#\")' ><span class='glyphicon glyphicon-cog'></span></button>"+
+            "<button class='btn btn-sm btn-default btn-primary tooltipster-grid' title='delete' onclick='df.delete(\"#:_id#\")' ><span class='glyphicon glyphicon-trash'></span></button></div>"},
         ],
         dataBound:function(){
             $(".tooltipster-grid").tooltipster({
